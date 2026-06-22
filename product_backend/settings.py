53ca9e9a8599f2e86f2e45accff690b926b53c61
Jayanithi_R <Jayanithi85@gmail.com>
2026-06-22 12:54:21 +0530

    Render deployment setup

--- product_backend/settings.py
+++ product_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q#a*p*+ql)4id#)(8%^bb5ghyw-a!005&n9f57txmq#fw25u6-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
